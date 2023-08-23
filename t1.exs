2b1939a5212ddc7daa3c610034839a6271b6a95f
ramikanag <129320686+ramikanag@users.noreply.github.com>
2023-08-23 08:55:49 +0530

    t1.exs

--- t1.exs
+++ t1.exs
@@ -1,14 +1,14 @@
 defmodule Mapp do
   start_time=System.os_time()
-  def sequential_map([head | tail], function) do
-    [function.(head) | sequential_map(tail, function)]
+  def sequential_map([head|tail],function) do
+    [function.(head)|sequential_map(tail,function)]
   end
-  def sequential_map([], function), do: []
+  def sequential_map([],function), do: []
   end_time=System.os_time()
   IO.puts("Total time for sequential map function=#{end_time-start_time}")
 
   start_time_parallel=System.os_time()
-  def parallel_map(list, function) do
+  def parallel_map(list,function) do
     list
     |> Enum.map(&Task.async(fn -> function.(&1) end))
     |> Enum.map(&Task.await/1)
@@ -16,15 +16,15 @@ defmodule Mapp do
   end_time_parallel=System.os_time()
   IO.puts("Total time for parallel map function=#{end_time_parallel-start_time_parallel}")
 
-  def display_result(list, function) do
+  def display_result(list,function) do
     result = sequential_map(list, function)
     IO.puts("Sequential map result: #{result}")
 
-    result2 = parallel_map(list, function)
+    result2 = parallel_map(list,function)
     IO.puts("Parallel map result: #{result2}")
   end
 end
 
 list = 1..10000
-function = fn x -> x * x end
-Mapp.display_result(list, function)
+function = fn x -> x*x end
+Mapp.display_result(list,function)
