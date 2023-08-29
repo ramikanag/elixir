a6f1ecfdae9a62a7120589b06175b069a7275422
ramikanag <129320686+ramikanag@users.noreply.github.com>
2023-08-29 13:25:32 +0530

    Map using task.exs

--- t1.exs
+++ t1.exs
@@ -1,30 +1,25 @@
 defmodule Mapp do
-  start_time=System.os_time()
-  def sequential_map([head|tail],function) do
-    [function.(head)|sequential_map(tail,function)]
+  def sequential_map([head | tail], function) do Enum.reverse([head|tail])
+    [function.(head) | sequential_map(tail, function)]
   end
-  def sequential_map([],function), do: []
-  end_time=System.os_time()
-  IO.puts("Total time for sequential map function=#{end_time-start_time}")
+  def sequential_map([], _function), do: []
 
-  start_time_parallel=System.os_time()
-  def parallel_map(list,function) do
+  def parallel_map(list, function) do
     list
-    |> Enum.map(&Task.async(fn -> function.(&1) end))
-    |> Enum.map(&Task.await/1)
+    |> Mapp.sequential_map(&Task.async(fn -> function.(&1) end))
+    |> Mapp.sequential_map(&Task.await/1)
   end
-  end_time_parallel=System.os_time()
-  IO.puts("Total time for parallel map function=#{end_time_parallel-start_time_parallel}")
 
-  def display_result(list,function) do
+  def display_result(list, function) do
+    start_time=System.monotonic_time()
     result = sequential_map(list, function)
+    IO.puts("Total time for sequential map function=#{System.monotonic_time()-start_time}")
     IO.puts("Sequential map result: #{result}")
 
-    result2 = parallel_map(list,function)
+    start_time_parallel=System.monotonic_time()
+    result2 = parallel_map(result, function)
+    IO.puts("Total time for parallel map function=#{System.monotonic_time()-start_time_parallel}")
     IO.puts("Parallel map result: #{result2}")
   end
 end
-
-list = 1..10000
-function = fn x -> x*x end
-Mapp.display_result(list,function)
+Mapp.display_result(1..10000, fn x -> x * x end)
