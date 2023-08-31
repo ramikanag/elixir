78cd04e3c8b920746d343340eccb381b6992544e
ramikanag <129320686+ramikanag@users.noreply.github.com>
2023-08-31 10:38:44 +0530

    t1_agent.exs

--- t1_agent.exs
+++ t1_agent.exs
@@ -12,7 +12,7 @@ defmodule Mapp do
   def put(pid, list) do
     sequential_map(list, fn x ->
       Agent.update(pid, fn state ->
-        Map.put(state, x, x * x)
+        Map.put(state,x,x*x)
     end)
   end
 
