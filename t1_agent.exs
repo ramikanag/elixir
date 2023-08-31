4ed90da115bdba651d0826571e50cfae5b50f6d6
ramikanag <129320686+ramikanag@users.noreply.github.com>
2023-08-31 10:30:17 +0530

    t1_agent.exs

---
+++ t1_agent.exs
@@ -0,0 +1,43 @@
+defmodule Mapp do
+  use Agent
+  def sequential_map([head|tail],function) do
+    [function.(head)|sequential_map(tail,function)]
+  end
+  def sequential_map([],_function), do: []
+
+  def start_link do
+    Agent.start_link(fn -> %{} end)
+  end
+
+  def put(pid, list) do
+    sequential_map(list, fn x ->
+      Agent.update(pid, &Map.put(&1,x,x*x))
+    end)
+    pid
+  end
+
+  def get(pid) do
+    Agent.get(pid, fn state ->state end)
+  end
+
+
+  def parallel_map(list,_function) do
+    pid=start_link()
+    pid=put(pid,list)
+  end
+
+  def display_result(list, function) do
+    start_time = System.monotonic_time()
+    result = sequential_map(list, function)
+    IO.puts("Sequential map result: #{result}")
+    IO.puts("Total time for sequential map function=#{System.monotonic_time()-start_time}")
+
+    start_time_parallel = System.monotonic_time()
+    pid=parallel_map(list, function)
+    result2 =get(pid)
+    IO.puts("Parallel map result: #{result2}")
+    IO.puts("Total time for parallel map function=#{System.monotonic_time()-start_time_parallel}")
+  end
+end
+
+Mapp.display_result(1..10000, fn x -> x * x end)
